ba30c5d2ab7aef791341cc3a7a91fc0dc64fb507
Satyarth Vaidya <satyarthvaidya@gmail.com>
2019-02-04 18:58:01 +0400

    Leetcode

--- RepeatedNTimes.py
+++ RepeatedNTimes.py
@@ -1,10 +1,5 @@
 class Solution:
     def repeatedNTimes(self, A):
-        """
-        :type A: List[int]
-        :rtype: int
-        """
-        
         A.sort()
         for i in range(0,len(A)):
             if(A[i+1]-A[i] == 0):
